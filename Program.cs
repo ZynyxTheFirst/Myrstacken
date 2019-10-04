7ea6b3f5a6747d1904aa8af0d282b3e419ebd4b6
Robin Westling <Zynyx@CND8261K83.learnet.se>
2019-10-04 17:43:00 +0200

    Tidying upp

--- Program.cs
+++ Program.cs
@@ -1,245 +1,254 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Myrstacken
+#pragma warning disable IDE0044
+
+/// <summary>
+///  private void Run(string name) //kan bara använda private kod inom den klassen den ligger i // () är paratmer listan, namnet plus listan kallas signatur. om man ska anropa metoden måste signaturen vara med
+/// </summary>
+
+
+class Program
 {
-	class Program
+	List<Myra> myror = new List<Myra>();
+	Myrstack myrstack = new Myrstack();
+
+
+	static void Main()
 	{
-		/*
-        const int max_length = 10;
-        Myra[] myror = new Myra[max_length];
-		*/
+		Program p = new Program();
+		p.Run();
+	}
 
-		List<Myra> myror = new List<Myra>();
+	void Run()
+	{
+		Console.ForegroundColor = ConsoleColor.Cyan;
+		Console.WriteLine("Welcome to Myrstacken! \nType 'help' for help");
+		Console.ResetColor();
 
-
-		static void Main()
+		while (true)
 		{
-			Program p = new Program();
-			p.Run();
-		}
+			Console.WriteLine("Enter command: ");
 
-		/*
-		Boolean isPresent(string name)
-		{
-			
-		}
-		*/
+			string input = Console.ReadLine();
+			string[] remove = input.Split(" ");
+			string[] add = input.Split(" ");
+			string[] search = input.Split(" ");
 
-		void Run()
-		{
-			Console.ForegroundColor = ConsoleColor.Cyan;
-			Console.WriteLine("Welcome to Myrstacken! \nType 'help' for help");
-			Console.ResetColor();
-
-			while (true)
+			if (add[0] == "add")
 			{
-				Console.WriteLine("Enter command: ");
-
-
-				string input = Console.ReadLine();
-				string[] remove = input.Split(" ");
-				string[] add = input.Split(" ");
-				if (add[0] == "add")
+				if (add.Length > 3)
 				{
-					if (add.Length > 3)
+					Console.ForegroundColor = ConsoleColor.Yellow;
+					Console.WriteLine("Syntax error, to many words");
+					Console.ResetColor();
+				}
+				else
+				{
+					try
+					{
+						//Myra myra = new Myra(add[1], int.Parse(add[2]).ToString());
+						//myror.Add(myra);
+						myrstack.Add(add[1], add[2]);
+					}
+					catch (MyraAlreadyExistException)
 					{
 						Console.ForegroundColor = ConsoleColor.Yellow;
-						Console.WriteLine("Syntax error, to many words");
+						Console.WriteLine("Myra already exists");
 						Console.ResetColor();
 					}
-					else
-					{
-						try
-						{
-
-							Myra myra = new Myra(add[1], int.Parse(add[2]).ToString());
-							myror.Add(myra);
-
-							/*
-							for (int i = 0; i < myror.Count; i++)
-                            {
-                                Myra myra = new Myra(add[1], int.Parse(add[2]).ToString());
-                                if (myror[i] == null)
-                                {
-                                    myror[i] = myra;
-                                    Console.ForegroundColor = ConsoleColor.Green;
-                                    Console.WriteLine("Succes!");
-                                    Console.ResetColor();
-                                    break;
-                                }
-                            }
-							*/
-						}
-						catch (Exception e)
-						{
-							Console.ForegroundColor = ConsoleColor.Yellow;
-							Console.WriteLine("Wrong input, try again");
-							Console.ResetColor();
-						}
-					}
-				}
-
-				else if (remove[0] == "remove")
-				{
-					if (remove.Length > 2)
+					catch (Exception e)
 					{
 						Console.ForegroundColor = ConsoleColor.Yellow;
-						Console.WriteLine("Syntax error, to many words");
+						Console.WriteLine("Wrong input, try again");
 						Console.ResetColor();
 					}
-					else
+				}
+			}
+
+			else if (search[0] == "find")
+			{
+
+			}
+
+			else if (remove[0] == "remove")
+			{
+				if (remove.Length > 2)
+				{
+					Console.ForegroundColor = ConsoleColor.Yellow;
+					Console.WriteLine("Syntax error, to many words");
+					Console.ResetColor();
+				}
+				else
+				{
+					try
 					{
-						try
+						for (int i = myror.Count - 1; i >= 0; i--)
 						{
-							for (int i = myror.Count - 1; i >= 0; i--)
-							{
-								if (myror[i].GetName().ToLower() == remove[1].ToLower())
-									myror.RemoveAt(i);
-							}
-						}
-						catch
-						{
-							Console.ForegroundColor = ConsoleColor.Yellow;
-							Console.WriteLine("Wrong input, try again");
-							Console.ResetColor();
+							if (myror[i].GetName().ToLower() == remove[1].ToLower())
+								myror.RemoveAt(i);
 						}
 					}
-				}
-
-
-				else if (input == "help")
-				{
-					Console.WriteLine("type 'exit' to exit \ntype 'addmyra' to add a myra \ntype listmyra to list all the myror \ntype list to list the amount of ants \ntype add [name] [legs] to add a myra");
-				}
-
-				else if (input == "addmyra")
-				{
-					Console.WriteLine("Enter the name of the myra: ");
-					string name = Console.ReadLine();
-
-					int numlegs = 0;
-					do
+					catch
 					{
-						Console.WriteLine("Enter the number of legs on myra " + name + ":");
+						Console.ForegroundColor = ConsoleColor.Yellow;
+						Console.WriteLine("Wrong input, try again");
+						Console.ResetColor();
+					}
+				}
+			}
 
-						string legs = Console.ReadLine();
-						try
-						{
-							numlegs = int.Parse(legs);
-							Myra myra = new Myra(name, legs);
-							myror.Add(myra);
-						}
-						catch (Exception e)
-						{
-							Console.ForegroundColor = ConsoleColor.Yellow;
-							Console.WriteLine("Wrong input, try again");
-							Console.ResetColor();
-						}
-					} while (numlegs == 0);
 
-					/*
+			else if (input == "help")
+			{
+				Console.WriteLine("type 'exit' to exit \ntype 'addmyra' to add a myra \ntype listmyra to list all the myror \ntype list to list the amount of ants \ntype add [name] [legs] to add a myra");
+			}
 
+			else if (input == "addmyra")
+			{
+				Console.WriteLine("Enter the name of the myra: ");
+				string name = Console.ReadLine();
+
+				int numlegs = 0;
+				do
+				{
 					Console.WriteLine("Enter the number of legs on myra " + name + ":");
 
 					string legs = Console.ReadLine();
 					try
 					{
-						int numlegs = int.Parse(legs);
-						for (int i = 0; i < myror.Length; i++)
-						{
-							Myra myra = new Myra(name, legs);
-							if (myror[i] == null)
-							{
-								myror[i] = myra;
-								break;
-							}	
-						}
+						numlegs = int.Parse(legs);
+						Myra myra = new Myra(name, legs);
+						myror.Add(myra);
 					}
 					catch (Exception e)
 					{
-						Console.WriteLine(e.GetType().ToString() + ": Error");
+						Console.ForegroundColor = ConsoleColor.Yellow;
+						Console.WriteLine("Wrong input, try again");
+						Console.ResetColor();
 					}
+				} while (numlegs == 0);
+			}
 
-					*/
+			else if (input == "list")
+			{
+				Console.WriteLine(myror.Count);
+			}
 
-				}
-
-				else if (input == "list")
+			else if (input == "listmyra")
+			{
+				for (int i = 0; i < myror.Count; i++)
 				{
-					Console.WriteLine(myror.Count);
-				}
-
-				else if (input == "listmyra")
-				{
-					for (int i = 0; i < myror.Count; i++)
+					if (myror[i] != null)
 					{
-						if (myror[i] != null)
-						{
-							Console.ForegroundColor = ConsoleColor.Cyan;
-							Console.WriteLine(myror[i].ToString());
-							Console.ResetColor();
-						}
-
+						Console.ForegroundColor = ConsoleColor.Cyan;
+						Console.WriteLine(myror[i].ToString());
+						Console.ResetColor();
 					}
-				}
-
-				else if (input == "exit")
-				{
-					Environment.Exit(0);
 
 				}
-				else
+			}
+
+			else if (input == "exit")
+			{
+				Environment.Exit(0);
+
+			}
+
+			else
+			{
+				Console.ForegroundColor = ConsoleColor.Yellow;
+				Console.WriteLine("Unknown command");
+				Console.ResetColor();
+			}
+
+			/*
+			switch (input)
+			{
+				case "exit":
+						break;
+				case "listmyra":
+					break;
+				default:
 				{
 					Console.ForegroundColor = ConsoleColor.Yellow;
 					Console.WriteLine("Unknown command");
-					Console.ResetColor();
+					Console.ResetColor();   
 				}
-
-				/*
-                switch (input)
-                {
-                    case "exit":
-                            break;
-                    case "listmyra":
-                        break;
-                    default:
-                    {
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.WriteLine("Unknown command");
-                        Console.ResetColor();   
-                    }
-                }
-                */
 			}
+			*/
+		}
+	}
+
+	class Myrstack
+	{
+		//Myra[] myror;
+		List<Myra> myror = new List<Myra>();
+
+		//private void Add(Myra myra)
+		//{
+		//	try
+		//	{
+		//		Myra myra = new Myra(add[1], int.Parse(add[2]).ToString());
+		//		myror.Add(myra);
+		//	}
+		//	catch (Exception e)
+		//	{
+		//		Console.ForegroundColor = ConsoleColor.Yellow;
+		//		Console.WriteLine("Wrong input, try again");
+		//		Console.ResetColor();
+		//	}
+		//}
+
+		public void Add(string name, string legs)
+		{
+			Myra myra = new Myra(name, legs);
+			CheckDuplicate(myra);
+			myror.Add(myra);
+			
 		}
 
-		class Myrstack
+
+		private void CheckDuplicate(Myra myra)
 		{
-			Myra[] myror;
+			for (int i = 0; i < myror.Count; i++)
+			{
+				if (myror[i] != null)
+				{
+					if (myror[i].GetName().ToLower() == myra.GetName().ToLower())
+					{
+						throw new MyraAlreadyExistException();
+					}
+				}
+			}
+		}
+	}
+
+	class MyraAlreadyExistException : Exception
+	{
+
+	}
+
+	class Myra
+	{
+		private string name;
+		private string legs;
+
+		public Myra(string name, string legs)
+		{
+			this.name = name;
+			this.legs = legs;
 		}
 
-		class Myra
+		public string GetName()
 		{
-			private string name;
-			private string legs;
+			return name;
+		}
 
-			public Myra(string name, string legs)
-			{
-				this.name = name;
-				this.legs = legs;
-			}
-
-			public string GetName()
-			{
-				return name;
-			}
-
-			public override string ToString()
-			{
-				name = name.Substring(0, 1).ToUpper() + name.Substring(1).ToLower();
-				return name + ", " + legs;
-			}
+		public override string ToString()
+		{
+			name = name.Substring(0, 1).ToUpper() + name.Substring(1).ToLower();
+			return name + ", " + legs;
 		}
 	}
 }
